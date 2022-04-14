27383746db3b84efe85bc81476022038b8be20a1
irina.petrovskaya <irina.petrovskaya@jetbrains.com>
2022-04-14 21:53:45 +0200

    remove the garbage

--- src/test/java/com/mytests/spring/jpa/springjpaqueries/SpringJpaQueriesApplicationTests.java
+++ src/test/java/com/mytests/spring/jpa/springjpaqueries/SpringJpaQueriesApplicationTests.java
@@ -1,13 +1,18 @@
 package com.mytests.spring.jpa.springjpaqueries;
 
+import com.mytests.spring.jpa.springjpaqueries.repositories.SampleRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringJpaQueriesApplicationTests {
 
+    @Autowired
+    SampleRepository repository;
     @Test
     void contextLoads() {
+        repository.findAll().forEach(e -> repository.delete(e));
     }
 
 }
